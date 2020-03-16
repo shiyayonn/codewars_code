8d28a12a901cbcc0a6d3ec858d30e5854fb3535e
Jayson Concepcion <jconcepcion@blastasia.com>
2020-03-16 11:22:20 +0800

    Solution for Scoreword

---
+++ C#/6 kyu/scorewords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace scorewords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
