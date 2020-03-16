3479cea9dc9ba04e5564419a9bcf81cb2768063f
Jayson Concepcion <jconcepcion@blastasia.com>
2020-03-16 14:40:03 +0800

    Solutions for scoreword and threeorfive

--- C#/6 kyu/scorewords/Program.cs
+++ C#/6 kyu/scorewords/Program.cs
@@ -6,7 +6,27 @@ namespace scorewords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine($"{High("a fgztfhudkxsjqbcbg zzxsktnyub")}");
+        }
+        public static string High(string s){
+        String[] words = s.Split(null);
+        int index = 0;
+        int max = 0;
+        for(int i = 0 ; i<words.Length;i++){
+            int sum = 0;
+            char[] temp = words[i].ToCharArray();
+                for(int o = 0; o <temp.Length;o++){
+                    sum+=(int)temp[o] - 96;
+                }
+                Console.WriteLine($"{words[i]} {sum}");
+                if(max<=sum){
+                    max=sum;
+                    index=i;
+                }
+        }
+
+        return words[index];
         }
     }
 }
