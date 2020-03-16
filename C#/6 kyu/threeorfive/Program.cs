e93caf5f827a22e8406913c3dc7c7945b5283e40
Jayson Concepcion <jconcepcion@blastasia.com>
2020-03-16 14:36:56 +0800

    Solutions for Three or Five and Score Words

--- C#/6 kyu/threeorfive/Program.cs
+++ C#/6 kyu/threeorfive/Program.cs
@@ -6,7 +6,27 @@ namespace threeorfive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{Solution(10)}");
+        }
+        public static int Solution(int value)
+        {
+            int sum = 0;
+            for (int i = 1; i < value; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    sum+=i;
+                }
+                else if (i % 3 == 0)
+                {
+                    sum+=i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sum+=i;
+                }
+            }
+            return sum;
         }
     }
 }
